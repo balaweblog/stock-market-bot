cf31213f7866a54810e3becd925eb204ba838e24
balamurugan <bala@balamurugans-Mac-mini.local>
2026-06-28 13:31:28 +0530

    code

--- email_service.py
+++ email_service.py
@@ -1,6 +1,7 @@
 import smtplib
 from email.mime.text import MIMEText
 from config import *
+import re
 
 def send_email(body, html=False):
     if html:
@@ -11,12 +12,19 @@ def send_email(body, html=False):
     msg["From"] = EMAIL_FROM
     msg["To"] = EMAIL_TO
 
+    EMAIL_TO = EMAIL_TO.replace("\n", "").replace("\r", "")
+
     recipients = [
         email.strip()
         for email in EMAIL_TO.split(",")
         if email.strip()
     ]
+    EMAIL_REGEX = r"^[^@\s]+@[^@\s]+\.[^@\s]+$"
 
+    recipients = [e for e in recipients if re.match(EMAIL_REGEX, e)]
+
+    print("RAW EMAIL_TO:", repr(EMAIL_TO))
+    print("RECIPIENTS:", recipients)
 
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
